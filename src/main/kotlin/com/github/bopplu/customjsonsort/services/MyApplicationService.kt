a988f3f9edf8180cef8bb84004cf3fafacbc975d
GitHub Action <action@github.com>
2021-10-08 12:46:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/bopplu/customjsonsort/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bopplu/customjsonsort/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bopplu.customjsonsort.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bopplu.customjsonsort.MyBundle
 
 class MyApplicationService {
 
